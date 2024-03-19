8806faff241d3d9ad5f0aa91756f033a8534bad2
Herman Mauricio Moreno <herman.moreno@ucp.edu.co>
2024-03-19 17:58:19 -0500

    Nuevos cambios 19 de marzo 2024

--- frontend_prueba_tecnica/frontend_prueba_tecnica/src/App.jsx
+++ frontend_prueba_tecnica/frontend_prueba_tecnica/src/App.jsx
@@ -1,21 +1,19 @@
-//importamos el router
+
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ShowLibro from './component_crud/ShowLibro';
-
 import EditLibro from './component_crud/EditLibro';
 import CreateLibro from './component_crud/CreateLibro';
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-            <Route path='/' element={ <ShowLibro />} />
-            <Route path='/edit/:id' element={ <EditLibro />} />
-            <Route path='/create' element={ <CreateLibro />} />
-        </Routes>
-      </BrowserRouter>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ShowLibro/>} />
+        <Route path="/edit/:id" element={<EditLibro/>} />
+        <Route path="/create" element={<CreateLibro/>} /> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend_prueba_tecnica/frontend_prueba_tecnica/src/component_crud/ShowLibro.jsx
+++ frontend_prueba_tecnica/frontend_prueba_tecnica/src/component_crud/ShowLibro.jsx
@@ -1,26 +1,29 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import React from 'react'
+import axios from "axios";
 
 const URI = 'http://localhost:8001/libros/';
 
 function ShowLibro() {
     const [libros, setLibros] = useState([]);
+    const [anioFiltro, setAnioFiltro] = useState('');
+    const [fechaInicio, setFechaInicio] = useState('');
+    const [fechaFin, setFechaFin] = useState('');
+
 
     useEffect(() => {
         getLibros();
     }, []);
 
-    // Obtener la lista de libros 
+    // Obtener la lista de libros
     const getLibros = async () => {
         try {
             const response = await axios.get(URI);
             setLibros(response.data);
         } catch (error) {
-            console.error("Error al obtener los libross:", error);
+            console.error("Error al obtener los libros:", error);
         }
-    }
+    };
 
     // Eliminar un libro
     const deleteLibro = async (id) => {
@@ -30,19 +33,56 @@ function ShowLibro() {
         } catch (error) {
             console.error("Error al eliminar el libro:", error);
         }
-    }
+    };
+
+    // Funcion para filtrar libros por año de publicación
+    const filterLibrosByAnio = (anio) => {
+        setAnioFiltro(anio);
+    };
 
     return (
         <div className="container-fluid py-1 px-3">
             <div className="container-fluid py-4">
                 <div className="container card p-4">
                     <div className="row">
-                        <h2>Prueba tecnica crud de la libreria</h2>
+                        <h2>Prueba tecnica CRUD de la libreria</h2>
                         <div className="col">
                             <Link to="/create" className="btn btn-primary mt-2 mb-2">
-                                <i className="fas fa-plus"></i> Agregar Libro Nuevo
+                                <i className="fas fa-plus"></i> Agregar libro 
                             </Link>
+                            
                             <div className="blog-table-container">
+                                <div className="mb-2">
+                                    <label htmlFor="fechaInicio" className="form-label">Fecha de inicio:</label>
+                                    <input
+                                        type="date"
+                                        id="fechaInicio"
+                                        className="form-control"
+                                        value={fechaInicio}
+                                        onChange={(e) => setFechaInicio(e.target.value)}
+                                    />
+                                </div>
+                                <div className="mb-2">
+                                    <label htmlFor="fechaFin" className="form-label">Fecha de fin:</label>
+                                    <input
+                                        type="date"
+                                        id="fechaFin"
+                                        className="form-control"
+                                        value={fechaFin}
+                                        onChange={(e) => setFechaFin(e.target.value)}
+                                    />
+                                </div>
+
+                                <div className="mb-2">
+                                    <label htmlFor="anioFiltro" className="form-label">Filtrar por anio:</label>
+                                    <input
+                                        type="text"
+                                        id="anioFiltro"
+                                        className="form-control"
+                                        value={anioFiltro}
+                                        onChange={(e) => filterLibrosByAnio(e.target.value)}
+                                    />
+                                </div>
                                 <table className="table">
                                     <thead className="table-primary">
                                         <tr>
@@ -54,22 +94,25 @@ function ShowLibro() {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {libros.map((libro) => (
-                                            <tr key={libro.id}>
-                                                <td>{libro.titulo}</td>
-                                                <td>{libro.autor}</td>
-                                                <td>{libro.anio_publicacion}</td>
-                                                <td>{libro.genero}</td>
-                                                <td>
-                                                    <Link to={`/edit/${libro.id}`} className="btn btn-info">
-                                                        <i className="fas fa-edit"></i> Editar Libro
-                                                    </Link>
-                                                    <button onClick={() => deleteLibro(libro.id)} className="btn btn-danger">
-                                                        <i className="fas fa-trash-alt"></i> Eliminar
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        ))}
+                                        {libros
+                                            .filter(libro => !anioFiltro || libro.anio_publicacion.toString() === anioFiltro)
+                                            .map((libro) => (
+                                                <tr key={libro.id}>
+                                                    <td>{libro.titulo}</td>
+                                                    <td>{libro.autor}</td>
+                                                    <td>{libro.anio_publicacion}</td>
+                                                    <td>{libro.genero}</td>
+                                                    <td>
+                                                        <Link to={`/edit/${libro.id}`} className="btn btn-info">
+                                                            <i className="fas fa-edit"></i> Editar libro
+                                                        </Link>
+                                                            
+                                                        <button onClick={() => deleteLibro(libro.id)} className="btn btn-danger">
+                                                            <i className="fas fa-trash-alt"></i> Eliminar
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            ))}
                                     </tbody>
                                 </table>
                             </div>
